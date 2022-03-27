f1d0303ae2b784fb44e9eec5be36fcb6ec25ced2
yannivain <yannivain@gmail.com>
2022-03-27 17:05:24 +0200

    Stepper

---
+++ app/src/components/DocumentCreationSteps.vue
@@ -0,0 +1,133 @@
+<template>
+  <v-stepper
+      v-model="step"
+      vertical
+  >
+    <v-stepper-step
+        :complete="step > 1"
+        step="1"
+    >
+      Notary
+<!--      <small>Summarize if needed</small>-->
+    </v-stepper-step>
+
+    <v-stepper-content step="1">
+      <v-card
+          color="grey lighten-1"
+          class="mb-12"
+          height="200px"
+      ></v-card>
+      <v-btn
+          color="primary"
+          @click="step = 2"
+      >
+        Continue
+      </v-btn>
+      <v-btn text>
+        Cancel
+      </v-btn>
+    </v-stepper-content>
+
+    <v-stepper-step
+        :complete="step > 2"
+        step="2"
+    >
+      Sellers
+    </v-stepper-step>
+
+    <v-stepper-content step="2">
+      <v-card
+          color="grey lighten-1"
+          class="mb-12"
+          height="200px"
+      ></v-card>
+      <v-btn
+          color="primary"
+          @click="step = 3"
+      >
+        Continue
+      </v-btn>
+      <v-btn text>
+        Cancel
+      </v-btn>
+    </v-stepper-content>
+
+    <v-stepper-step
+        :complete="step > 3"
+        step="3"
+    >
+      Buyers
+    </v-stepper-step>
+
+    <v-stepper-content step="3">
+      <v-card
+          color="grey lighten-1"
+          class="mb-12"
+          height="200px"
+      ></v-card>
+      <v-btn
+          color="primary"
+          @click="step = 4"
+      >
+        Continue
+      </v-btn>
+      <v-btn text>
+        Cancel
+      </v-btn>
+    </v-stepper-content>
+
+    <v-stepper-step step="4">
+      Deed
+    </v-stepper-step>
+    <v-stepper-content step="4">
+      <v-card
+          color="grey lighten-1"
+          class="mb-12"
+          height="200px"
+      ></v-card>
+      <v-btn
+          color="primary"
+          @click="step = 5"
+      >
+        Continue
+      </v-btn>
+      <v-btn text>
+        Cancel
+      </v-btn>
+    </v-stepper-content>
+
+    <v-stepper-step step="4">
+      Other
+    </v-stepper-step>
+    <v-stepper-content step="4">
+      <v-card
+          color="grey lighten-1"
+          class="mb-12"
+          height="200px"
+      ></v-card>
+      <v-btn
+          color="primary"
+          @click="step = 1"
+      >
+        Continue
+      </v-btn>
+      <v-btn text>
+        Cancel
+      </v-btn>
+    </v-stepper-content>
+  </v-stepper>
+</template>
+
+<script>
+export default {
+  name: 'DocumentCreationSteps',
+  components: {},
+  data: () => ({
+    step: 1
+  })
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ app/src/components/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <document-creation-steps></document-creation-steps>
+  </div>
+</template>
+
+<script>
+
+import DocumentCreationSteps from "@/components/DocumentCreationSteps"
+
+export default {
+  name: 'HomeView',
+  components: {DocumentCreationSteps},
+  data: () => ({
+    step: 1
+  })
+};
+</script>
+
+<style scoped>
+
+</style>
